f32f6f3fd68b67bea1c16ee1cce8c3c47e0351dd
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-12-01 10:46:15 +0200

    forms workshop - useRef

--- react-my-pets/src/App.js
+++ react-my-pets/src/App.js
@@ -4,15 +4,18 @@ import Header from './components/Header/Header';
 import { Route, Routes } from 'react-router-dom';
 import Categories from './components/Categories/Categories';
 import PetDetails from './components/PetDetails/PetDetails';
+import DemoPage from './components/Demo';
 
 function App() {
   return (
     <div className="container">
       <Header />
+
       <Routes>
         <Route path="/" element={<Categories />} />
         <Route path="/categories/:category" element={<Categories />} />
         <Route path="/pets/details/:petId" element={<PetDetails />} />
+        <Route path='/demo' element={<DemoPage />} />
       </Routes>
       <Footer />
     </div>
