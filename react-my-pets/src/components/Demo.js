de1dc83f4cf21f58db27d73f53c41ca93a1740be
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-12-02 09:30:32 +0200

    learn useState react hook again

--- react-my-pets/src/components/Demo.js
+++ react-my-pets/src/components/Demo.js
@@ -4,87 +4,36 @@ import classes from './Demo.module.css';
 
 
 const Demo = () => {
-	const [nameInput, setNameInput] = useState('');
-	const [ageInput, setAgeInput] = useState('');
-	const [bioInput, setBioInput] = useState('bio');
-	const [optionInput, setOptionInput] = useState('unemployed');
-	const [emailInput, setEmailInput] = useState('');
-	const [error, setError] = useState(false);
+	const [state, setState] = useState({
+		count: 11,
+		step: 1
+	});
 
-	const emailInputRef = useRef();
+	const onCounterButtonClickHandler = (e) => {
+		setState((oldState) => ({ ...oldState, count: oldState.count + oldState.step }));
 
-	const onEmailChange = (e) => {
-		if (!emailInputRef.current.value.includes('@')) {
-			setError(true);
-			console.log('error');
-			emailInputRef.current.focus();
-		}
-		setEmailInput(emailInputRef.current.value);
 	};
 
+	const onStepSelectChangeHandler = (e) => {
+		const stepValue = Number(e.target.value);
 
-	const onSubmitHandler = (e) => {
-		e.preventDefault();
-		console.log(nameInput, ageInput);
-	};
-
-	const onUsernameChangeHandler = (e) => {
-		setNameInput(e.target.value);
-	};
-
-	const onAgeChangeHandler = (e) => {
-		setAgeInput(e.target.value);
-	};
-
-	const onBioChangeHandler = (e) => {
-		setBioInput(e.target.value);
-	};
-
-	const onOptionChangeHanler = (e) => {
-		setOptionInput(e.target.value);
+		setState(oldState => ({ ...oldState, step: stepValue }));
 	};
 
 
 	return (
-		<div style={{ display: 'flex', flexFlow: 'column' }}>
-			<h1>Demo Form</h1>
-			<form onSubmit={onSubmitHandler} >
-				<div>
-					<label htmlFor="username">Username</label>
-					<input value={nameInput}
-						onChange={onUsernameChangeHandler}
-						type="text"
-						name="username"
-						id="username"
-					/>
-				</div>
-
-				<div>
-					<label htmlFor="email">Email</label>
-					<input onChange={onEmailChange} type="email" name="email" id="email" ref={emailInputRef} />
-				</div>
-				<div className={`input-validation ${error && classes.show}`}>Invalid Email</div>
-
-				<div>
-					<label htmlFor="age">Age</label>
-					<input value={ageInput} onChange={onAgeChangeHandler} type="number" name="age" id="age" />
-				</div>
-
-				<div>
-					<label htmlFor="bio">Bio</label>
-					<textarea value={bioInput} name="bio" onChange={onBioChangeHandler} id="bio" />
-				</div>
-
-				<div>
-					<label htmlFor="occupation">Occupation</label>
-					<select onChange={onOptionChangeHanler} name="occupation" id="occupatio" value={optionInput}>
-						<option value="it">IT</option>
-						<option value="engineer">Engineer</option>
-						<option value="unemployed">Unemployed</option>
-					</select>
-				</div>
-				<input type="submit" value="Submit" />
-			</form>
+		<div>
+			<h1>Counter</h1>
+			<div>{count}</div>
+			<button onClick={onCounterButtonClickHandler}>Increment
+			</button>
+			<label htmlFor="step">Step</label>
+			<select onChange={onStepSelectChangeHandler} name="step" id="step">
+				<option value="1"></option>
+				<option value="2"></option>
+				<option value="3"></option>
+				<option value="4"></option>
+			</select>
 		</div>
 	);
 };
