9630502f6baf09f5ba0af1dc5c91df07ab57b820
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-12-02 10:55:08 +0200

    great custom hook for http request only for get method tho

--- react-my-pets/src/components/Demo.js
+++ react-my-pets/src/components/Demo.js
@@ -1,23 +1,42 @@
-import { useRef, useState } from "react";
-
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 import classes from './Demo.module.css';
 
 
 const Demo = () => {
-	const [state, setState] = useState({
-		count: 11,
-		step: 1
-	});
+	// const [state, setState] = useState({
+	// 	count: 11,
+	// 	step: 1
+	// });
+
+
+	const [count, setCount] = useState(1);
+	const [step, setStep] = useState(1)
+	// const [character, setCharacter] = useState({ name: "Nobody" });
+
+	const [character, isLoading] = useFetch(`https://swapi.dev/api/people/${step}`, {});
+	// useEffect(() => {
+	// 	console.log('useeffect')
+	// 	fetch(`https://swapi.dev/api/people/${step}`)
+	// 		.then(res => res.json())
+	// 		.then(char => setCharacter(char));
+
+	// 	return () => {
+	// 		console.log('cleanup func')
+	// 	};
+	// }, [step]);
+	;
 
 	const onCounterButtonClickHandler = (e) => {
-		setState((oldState) => ({ ...oldState, count: oldState.count + oldState.step }));
-
+		// setState((oldState) => ({ ...oldState, count: oldState.count + oldState.step }));
+		setCount(oldState => oldState + step);
 	};
 
 	const onStepSelectChangeHandler = (e) => {
 		const stepValue = Number(e.target.value);
-
-		setState(oldState => ({ ...oldState, step: stepValue }));
+		// setState(oldState => ({ ...oldState, step: stepValue }));
+		setStep(stepValue);
 	};
 
 
@@ -27,13 +46,18 @@ const Demo = () => {
 			<div>{count}</div>
 			<button onClick={onCounterButtonClickHandler}>Increment
 			</button>
+			{isLoading && <div>LOADING...</div>}
+			<h1>{character?.name}</h1>
 			<label htmlFor="step">Step</label>
 			<select onChange={onStepSelectChangeHandler} name="step" id="step">
-				<option value="1"></option>
-				<option value="2"></option>
-				<option value="3"></option>
-				<option value="4"></option>
+				<option value="1">1</option>
+				<option value="2">2</option>
+				<option value="3">3</option>
+				<option value="4">4</option>
 			</select>
+			<div>
+				<Link to="/">Home</Link>
+			</div>
 		</div>
 	);
 };
